e3e0d0b04d3c201413e2169f4f0b41f768918588
Jinsu <jinsu8828@gmail.com>
2024-10-10 12:14:36 +0900

    add

--- src/main/java/com/example/myboard/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.myboard.repository;
-
-import com.example.myboard.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
